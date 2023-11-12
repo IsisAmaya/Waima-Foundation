f16714290d3a0625fd802f17bf2c94bf1d0ab509
andrescaro16 <julicaro2003@gmail.com>
2023-11-11 20:24:03 -0500

    project base
    This is the old version of the project

---
+++ Comidas/models.py
@@ -0,0 +1,15 @@
+
+from django.db import models
+from django import forms
+# Create your models here.
+class comida(models.Model):
+    producto = models.TextField(max_length=50)
+    descripcion = models.TextField(max_length=5000)
+    peso= models.IntegerField(max_length=100)
+    informacion = models.TextField(max_length=4000)
+    FechaIngreso= models.DateField(null=True)
+    
+    
+    def _str_(self):
+        return self.producto 
+
